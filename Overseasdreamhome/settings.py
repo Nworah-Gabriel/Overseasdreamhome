dcddbc9b75d550868a51e3fac2935356db08e0be
Nworah-Gabriel <gabrielnworah6@gmail.com>
2023-06-08 07:01:16 +0100

    changed my configuration setting in the settings.py file

--- Overseasdreamhome/settings.py
+++ Overseasdreamhome/settings.py
@@ -120,7 +120,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ecommerceApp/static'),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
